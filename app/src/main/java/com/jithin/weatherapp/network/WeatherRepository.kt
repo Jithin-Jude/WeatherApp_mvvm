e07d8fdadfaabe95e96a254baf3d691d7046a371
jithin825427 <jithin@thecalmsleep.com>
2024-03-19 11:44:01 +0530

    minor business logic update

--- app/src/main/java/com/jithin/weatherapp/network/WeatherRepository.kt
+++ app/src/main/java/com/jithin/weatherapp/network/WeatherRepository.kt
@@ -2,12 +2,7 @@ package com.jithin.weatherapp.network
 
 // WeatherRepository.kt
 
-import android.util.Log
-import com.jithin.weatherapp.getDayOfWeek
-import com.jithin.weatherapp.kelvinToCelsius
 import com.jithin.weatherapp.model.CurrentWeatherData
-import com.jithin.weatherapp.model.WeatherForecastData
-import com.jithin.weatherapp.model.WeatherForecastDay
 import kotlinx.coroutines.flow.flow
 import retrofit2.Retrofit
 import javax.inject.Inject
@@ -39,35 +34,14 @@ class WeatherRepository @Inject constructor(private val retrofit: Retrofit) {
         }
     }
 
-    suspend fun getForecastData(city: String) = flow<DataState<WeatherForecastData>> {
+    suspend fun getForecastData(city: String) = flow<DataState<List<WeatherData>>> {
         try {
             emit(DataState.Loading)
             val response = apiService.getForecast(city)
             if (response.isSuccessful) {
                 val weatherForecastResponse = response.body()
                 weatherForecastResponse?.let {
-                    val groupedByDay = weatherForecastResponse.weatherList.groupBy {
-                        getDayOfWeek(it.dt)
-                    }
-                    val listOfDays = groupedByDay.map { (day, forecasts) ->
-                        val avgTemperature = forecasts.map { it.temperature.temp }.average()
-                        WeatherForecastDay(day, kelvinToCelsius(avgTemperature))
-                    }
-                    // Current day
-                    val today = getDayOfWeek(System.currentTimeMillis() / 1000)
-                    Log.d("TAG", "TODAY :=> ${today}")
-                    // Find the index of today in the list
-                    val nextDayIndex = listOfDays.indexOfFirst { it.day == today } + 1
-                    // Get the sublist containing the next 4 days
-                    val nextFourDays = if (nextDayIndex > 0) {
-                        // If today is found in the list, get the next 4 days
-                        listOfDays.subList(nextDayIndex, minOf(nextDayIndex + 4, listOfDays.size))
-                    } else {
-                        // If today is not found in the list, start from the first day
-                        listOfDays.subList(0, minOf(4, listOfDays.size))
-                    }
-                    val data = WeatherForecastData(days = nextFourDays)
-                    emit(DataState.Success(data))
+                    emit(DataState.Success(weatherForecastResponse.weatherList))
                 } ?: kotlin.run {
                     emit(DataState.Error(Exception(response.message())))
                 }
--- app/src/main/java/com/jithin/weatherapp/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/jithin/weatherapp/viewmodel/WeatherViewModel.kt
@@ -1,12 +1,17 @@
 package com.jithin.weatherapp.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jithin.weatherapp.getDayOfWeek
+import com.jithin.weatherapp.kelvinToCelsius
 import com.jithin.weatherapp.model.CurrentWeatherData
 import com.jithin.weatherapp.model.WeatherForecastData
+import com.jithin.weatherapp.model.WeatherForecastDay
 import com.jithin.weatherapp.network.DataState
+import com.jithin.weatherapp.network.WeatherData
 import com.jithin.weatherapp.network.WeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -52,7 +57,8 @@ class WeatherViewModel @Inject constructor(private val repository: WeatherReposi
                     }
 
                     is DataState.Success -> {
-                        _weatherForecastData.postValue(result.data)
+                        val data = filterForecastDays(result.data)
+                        _weatherForecastData.postValue(data)
                         _loader.postValue(false)
                     }
 
@@ -63,5 +69,29 @@ class WeatherViewModel @Inject constructor(private val repository: WeatherReposi
                 }
             }
         }
+    }
+
+    private fun filterForecastDays(data: List<WeatherData>): WeatherForecastData {
+        val groupedByDay = data.groupBy {
+            getDayOfWeek(it.dt)
+        }
+        val listOfDays = groupedByDay.map { (day, forecasts) ->
+            val avgTemperature = forecasts.map { it.temperature.temp }.average()
+            WeatherForecastDay(day, kelvinToCelsius(avgTemperature))
+        }
+        // Current day
+        val today = getDayOfWeek(System.currentTimeMillis() / 1000)
+        Log.d("TAG", "TODAY :=> ${today}")
+        // Find the index of today in the list
+        val nextDayIndex = listOfDays.indexOfFirst { it.day == today } + 1
+        // Get the sublist containing the next 4 days
+        val nextFourDays = if (nextDayIndex > 0) {
+            // If today is found in the list, get the next 4 days
+            listOfDays.subList(nextDayIndex, minOf(nextDayIndex + 4, listOfDays.size))
+        } else {
+            // If today is not found in the list, start from the first day
+            listOfDays.subList(0, minOf(4, listOfDays.size))
+        }
+        return WeatherForecastData(days = nextFourDays)
     }
 }
